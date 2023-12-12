28408c4153c4d756f4fb4f36afd4b4fd67890556
Leandro Tittarelli <leandrotittarelli@gmail.com>
2023-12-12 16:31:47 -0300

    :lipstick: App  base style

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,15 @@ import './App.css'
 
 function App() {
   return (
-    <h1>Batalla Naval</h1>
+    <main>
+      <section className='header'>
+        <h1>Batalla Naval</h1>
+      </section>
+      <section className='game'></section>
+      <footer>
+        <p>Desarrollado por <a href='https://github.com/Tittarelli'>Leandro Tittarelli</a> en 2023 para la materia Construcción de Interfaces de Usuario de la UNQ</p>
+      </footer>
+    </main>
   )
 }
 
